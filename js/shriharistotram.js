1d7c279c99420f7dae1f68702559dedb6f60b9eb
मयंक <mastermayank91@gmail.com>
2024-02-10 16:37:17 +0000

    Add js/*

---
+++ js/shriharistotram.js
@@ -0,0 +1,62 @@
+/*
+-   
+-   Designed | Develped by MayankDevil
+-   https://github.com/MayankDevil/sanskrti
+-   JavaScript ./js/shriharistotram.js
+*/
+try
+{
+    /*
+        API collection set { shlok | arth | parivartan }
+    */ 
+    var shriharistotram = [
+        {
+            shlok : `जगज्जालपालं चलत्कण्ठमालं शरच्चन्द्रभालं महादैत्यकालं नभोनीलकायं दुरावारमायं सुपद्मासहायम् भजेऽहं भजेऽहं ॥१॥`,
+            arth : `मैं चलती हुई गर्दनों की माला वाले ब्रह्मांड के जाल के रक्षक की पूजा करता हूं, जो शरद ऋतु के चंद्रमा को धारण करता है, जो महान राक्षसों का समय है, जिसका शरीर आकाश में नीला है, जिस तक पहुंचना मुश्किल है, जिसे सुंदर कमल द्वारा मदद मिलती है, मैं उसकी पूजा करता हूं उसे।`,
+            parivartan : `I worship the protector of the web of the universe with the garland of moving necks, who holds the autumn moon, the time of the great demons, whose body is blue in the sky, difficult to reach, who is adorned with beautiful lotuses. Helped by him, I worship him.`
+        },
+        {
+            shlok : `सदाम्भोधिवासं गलत्पुष्पहासं जगत्सन्निवासं शतादित्यभासं गदाचक्रशस्त्रं लसत्पीतवस्त्रं हसच्चारुवक्त्रं भजेऽहं भजेऽहं ॥२॥ `,
+            arth : `मैं उनकी पूजा करता हूं जो अनंत सागर में रहते हैं, जिनकी मुस्कान फूल की तरह है, जो ब्रह्मांड का निवास स्थान है, जो सैकड़ों सूर्यों की तरह चमकते हैं, जो गदाओं और चक्रों से लैस हैं, जो चमकदार पीले वस्त्र पहने हुए हैं और जिनका सुंदर चेहरा मुस्कुरा रहा है, मैं उनकी पूजा करता हूं उसे।`,
+            parivartan : `I worship Him who lives in the infinite ocean, whose smile is like a flower, who is the abode of the universe, who shines like hundreds of suns, who is armed with maces and discs, who is wearing bright yellow robes and I worship those whose beautiful face is smiling.`
+        },
+        {
+            shlok : `रमाकण्ठहारं श्रुतिव्रातसारं जलान्तर्विहारं धराभारहारं चिदानन्दरूपं मनोज्ञस्वरूपं ध्रुतानेकरूपं भजेऽहं भजेऽहं ॥३॥ `,
+            arth : `मैं भाग्य की देवी के उस हार की पूजा करता हूं, सुनने के भँवर का सार, पानी के बीच में खेलता हुआ वह हार, पृथ्वी के बोझ का वह हार, चैतन्य आनंद का वह स्वरूप, जो शीघ्र ही कई रूप धारण कर लेता है, मैं उसकी पूजा करता हूं।`,
+            parivartan : `I worship that necklace of the goddess of fortune, the essence of the whirlpool of hearing, that necklace playing in the midst of water, that necklace of the burden of the earth, that form of conscious bliss that quickly takes many forms, i worship him`
+        },
+        {
+            shlok : `जराजन्महीनं परानन्दपीनं समाधानलीनं सदैवानवीनं जगज्जन्महेतुं सुरानीककेतुं त्रिलोकैकसेतुं भजेऽहं भजेऽहं ॥४॥`,
+            arth : `मैं बुढ़ापे और जन्म से रहित परम आनंद से परिपूर्ण, संतुष्टि में लीन, ब्रह्मांड के जन्म का नया कारण, देवताओं की सेना का सितारा और तीनों लोकों के लिए एकमात्र पुल की पूजा करता हूं जिसकी मैं पूजा करता हूं।`,
+            parivartan : `I worship Him, full of supreme bliss, devoid of old age and birth, absorbed in satisfaction, the new cause of the birth of the universe, the star of the army of gods and the only bridge to the three worlds.`
+        },
+        {
+            shlok : `कृताम्नायगानं खगाधीशयानं विमुक्तेर्निदानं हरारातिमानं स्वभक्तानुकूलं जगद्व्रुक्षमूलं निरस्तार्तशूलं भजेऽहं भजेऽहं ॥५॥ `,
+            arth : `जो वेदों की ऋचाएँ गाते हैं, जो पक्षियों के सिंहासन पर विराजमान हैं, जो मुक्ति के निदान हैं, जो सबसे महान हैं और जो अपने भक्तों के अनुकूल हैं, मैं उनकी पूजा करता हूँ।`,
+            parivartan : `I worship Him who sings the hymns of the Vedas, who sits on the throne of birds, who is the source of liberation, who is the greatest of all and who is friendly to His devotees.`
+        },
+        {
+            shlok : `समस्तामरेशं द्विरेफाभकेशं जगद्विम्बलेशं ह्रुदाकाशदेशं सदा दिव्यदेहं विमुक्ताखिलेहं सुवैकुण्ठगेहं भजेऽहं भजेऽहं ॥६॥ `,
+            arth : `मैं उन सभी देवताओं के स्वामी की पूजा करता हूं जिनके बाल दो पंखों के समान हैं, जो ब्रह्मांड के स्वामी हैं, जिनका हृदय आकाश में है, जिनका शरीर हमेशा मुक्त है, जिनका पूरा शरीर मुक्त है और जिनका निवास सुंदर वैकुंठ है।`,
+            parivartan : `I worship the Lord of all the gods whose hair is like two feathers, who is the lord of the universe, whose heart is in the sky, whose body is always free, whose entire body is free and whose abode is the beautiful Vaikuntha.`
+        },
+        {
+            shlok : `सुरालिबलिष्ठं त्रिलोकीवरिष्ठं गुरूणां गरिष्ठं स्वरूपैकनिष्ठं सदा युद्धधीरं महावीरवीरं महाम्भोधितीरं भजेऽहं भजेऽहं ॥७॥ `,
+            arth : `मैं उनकी पूजा करता हूं जो शराब से भी अधिक शक्तिशाली हैं और जो तीनों लोकों में सर्वश्रेष्ठ हैं, जो आध्यात्मिक गुरुओं में सर्वश्रेष्ठ हैं, जो युद्ध में हमेशा साहसी हैं, जो महान वीरों के नायक हैं, जो महान महासागर के तट पर हैं, मैं उनकी पूजा करता हूं .`,
+            parivartan : `I worship him who is more powerful than wine and who is the best in the three worlds, who is the best among spiritual masters, who is always courageous in battle, who is the hero of great heroes, who is on the shores of the great ocean, I I worship him.`
+        },
+        {
+            shlok : `रमावामभागं तलानग्रनागं कृताधीनयागं गतारागरागं मुनीन्द्रैः सुगीतं सुरैः संपरीतं गुणौधैरतीतं भजेऽहं भजेऽहं ॥८॥ `,
+            arth : `मैं राम के वामांग के उस निचले सिरे वाले नाग की पूजा करता हूं, जिसने अपने वश में होकर यज्ञ किया है, जिसने सभी वासनाओं को खो दिया है और जो महान ऋषियों द्वारा अच्छी तरह से गाया जाता है और जो देवताओं से घिरा हुआ है, जो गुणों के सागर में पारंगत है। पूजा करना।`,
+            parivartan : `I worship the serpent at the lower end of the Vamanga of Rama, who has performed the yagya under his control, who has lost all desires and who is well-sung by the great sages and who is surrounded by the gods, Who is well versed in the ocean of virtues. worship.`
+        }
+    ]
+    // इति श्रीरावणकृतं शिवताण्डवस्तोत्रं सम्पूर्णम्।
+    sumiran("shriharistotram", shriharistotram)
+
+    document.title = `MayankDevil`
+}
+catch (error)
+{
+    console.log(`${ error }`)    
+}
